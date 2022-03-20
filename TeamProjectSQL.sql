d6e5b3f76b73dddf0b16fec671c38192901b5419
jackeline Gonzales B <jackiegb@jackelines-Mini.attlocal.net>
2022-03-19 22:20:49 -0400

    Jupyter file added

--- TeamProjectSQL.sql
+++ TeamProjectSQL.sql
@@ -1,3 +1,133 @@
+DROP TABLE IF EXISTS Customers_sheet;
+DROP TABLE IF EXISTS Products_sheet;
+DROP TABLE IF EXISTS Regions_sheet;
+--DROP TABLE IF EXISTS Sales_orders_sheet;
+DROP TABLE IF EXISTS Sales_team_sheet;
+DROP TABLE IF EXISTS Store_locations_sheet;
+DROP TABLE IF EXISTS Walmart_retail_data;
+
+CREATE TABLE Walmart_retail_data (
+	city VARCHAR NOT NULL,
+	customer_name VARCHAR NOT NULL,
+	customer_segment VARCHAR NOT NULL,
+	discount DEC NOT NULL,
+	number_of_records INT NOT NULL,
+	order_date DATE NOT NULL,
+	order_id INT NOT NULL,
+	order_priority VARCHAR NOT NULL,
+	order_quantity INT NOT NULL,
+	product_base_margin DEC,
+	product_category VARCHAR NOT NULL,
+	product_container VARCHAR NOT NULL,
+	--product_name VARCHAR NOT NULL,
+	--product_sub_category VARCHAR NOT NULL,
+	profit DEC NOT NULL,
+	region VARCHAR NOT NULL,
+	row_id INT NOT NULL,
+	sales DEC NOT NULL, 
+	ship_date DATE NOT NULL,
+	ship_mode VARCHAR NOT NULL,
+	shipping_cost DEC NOT NULL,
+	state VARCHAR NOT NULL, 
+	unit_price DEC NOT NULL,
+	zip_code INT NOT NULL,
+	CONSTRAINT pk_Walmart_retail_data PRIMARY KEY (
+        row_id
+     )
+);
+
+CREATE TABLE Sales_orders_sheet (
+    ordernumber VARCHAR   NOT NULL,
+    sales_channel VARCHAR   NOT NULL,
+    warehousecode VARCHAR   NOT NULL,
+    procureddate DATE   NOT NULL,
+    orderdate DATE   NOT NULL,
+    shipdate DATE   NOT NULL,
+    deliverydate DATE   NOT NULL,
+    currencycode VARCHAR   NOT NULL,
+    salesteamid INT   NOT NULL,
+    customerid INT   NOT NULL,
+    storeid INT   NOT NULL,
+    productid INT   NOT NULL,
+    order_quantity INT   NOT NULL,
+    discount_applied DEC   NOT NULL,
+    unit_price DEC   NOT NULL,
+    unit_cost DEC   NOT NULL,
+    CONSTRAINT pk_Sales_orders_sheet PRIMARY KEY (
+        ordernumber
+     )
+);
+
+CREATE TABLE Customers_sheet (
+    customerid INT   NOT NULL,
+    customer_names VARCHAR   NOT NULL,
+    CONSTRAINT pk_Customers_sheet PRIMARY KEY (
+        customerid
+     )
+);
+
+CREATE TABLE Store_locations_sheet (
+    storeid INT   NOT NULL,
+    city_name VARCHAR   NOT NULL,
+    county VARCHAR   NOT NULL,
+    statecode VARCHAR   NOT NULL,
+    state VARCHAR   NOT NULL,
+    type VARCHAR   NOT NULL,
+    latitude DEC   NOT NULL,
+    longitude DEC   NOT NULL,
+    --location POINT   NOT NULL,
+    areacode INT   NOT NULL,
+    population INT   NOT NULL,
+    household_income INT   NOT NULL,
+    median_income INT   NOT NULL,
+    land_area INT   NOT NULL,
+    water_area INT   NOT NULL,
+    time_zone VARCHAR   NOT NULL,
+    CONSTRAINT pk_Store_locations_sheet PRIMARY KEY (
+        storeid
+    )
+);
+
+CREATE TABLE Products_sheet (
+    productid INT   NOT NULL,
+    product_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Products_sheet PRIMARY KEY (
+        productid
+     )
+);
+
+CREATE TABLE Regions_sheet (
+    statecode VARCHAR   NOT NULL,
+    state VARCHAR   NOT NULL,
+    region VARCHAR   NOT NULL,
+    CONSTRAINT pk_Regions_sheet PRIMARY KEY (
+        statecode
+     )
+);
+
+CREATE TABLE Sales_team_sheet (
+    salesteamid INT   NOT NULL,
+    sales_team VARCHAR   NOT NULL,
+    region VARCHAR   NOT NULL,
+    CONSTRAINT pk_Sales_team_sheet PRIMARY KEY (
+        salesteamid
+     )
+);
+
+ALTER TABLE Sales_orders_sheet ADD CONSTRAINT fk_Sales_orders_sheet_salesteamid FOREIGN KEY(salesteamid)
+REFERENCES Sales_team_sheet (salesteamid);
+
+ALTER TABLE Sales_orders_sheet ADD CONSTRAINT fk_Sales_orders_sheet_customerid FOREIGN KEY(customerid)
+REFERENCES Customers_sheet (customerid);
+
+ALTER TABLE Sales_orders_sheet ADD CONSTRAINT fk_Sales_orders_sheet_storeid FOREIGN KEY(storeid)
+REFERENCES Store_locations_sheet (storeid);
+
+ALTER TABLE Sales_orders_sheet ADD CONSTRAINT fk_Sales_orders_sheet_productid FOREIGN KEY(productid)
+REFERENCES Products_sheet (productid);
+
+
+
 select *
 From Store_locations_sheet;
 
